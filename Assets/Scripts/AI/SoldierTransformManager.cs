7ad5442acc562d2b7df2e48ccb583fefcb409e0e
amarillo-sebas <amarillo.sebas@gmail.com>
2018-05-22 22:23:34 -0500

    AI Basic Setup
    -AI foundations
    -Soldier AI for both factions is configured in one single behavior tree
    -Soldier stats are configured in one single scriptable object
    -Lightmap baked but has some blocky shadows

---
+++ Assets/Scripts/AI/SoldierTransformManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime;
+
+public class SoldierTransformManager : MonoBehaviour {
+	public BehaviorTree behaviorTree;
+
+	public SoldierStats statsInfo;
+
+	public Transform target;
+	public bool targetSighted;
+
+	void Start () {
+		behaviorTree.SetVariableValue("movementSpeed", statsInfo.movementSpeed);
+		behaviorTree.SetVariableValue("rotationSpeed", statsInfo.rotationSpeed);
+		behaviorTree.SetVariableValue("minWanderDistance", statsInfo.minWanderDistance);
+		behaviorTree.SetVariableValue("maxWanderDistance", statsInfo.maxWanderDistance);
+		behaviorTree.SetVariableValue("minWanderPause", statsInfo.minWanderPause);
+		behaviorTree.SetVariableValue("maxWanderPause", statsInfo.maxWanderPause);
+		behaviorTree.SetVariableValue("wanderRate", statsInfo.wanderRate);
+	}
+	
+	void Update () {
+		if (targetSighted) {
+			if (target) {
+				Quaternion newRotation =  Quaternion.RotateTowards (
+					transform.rotation,
+					Quaternion.LookRotation(target.position - transform.position),
+					Time.deltaTime * statsInfo.rotationSpeed
+				);
+
+				newRotation.eulerAngles = new Vector3 (
+					0f, -newRotation.eulerAngles.y, 0f
+				);
+
+				transform.rotation = newRotation;
+			}
+		}
+	}
+}
